9ea6b59a4e5ae0d24fd99baf736e6d8c3c223e88
Jos Huybrighs <jhuybrighs@hotmail.com>
2026-02-12 14:35:48 +0100

    Add project files.

---
+++ ScrollViewMauiApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace ScrollViewMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnHideKeyboardButtonClicked(object sender, EventArgs e)
+        {
+#if ANDROID
+        var activity = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity;
+        if (activity?.CurrentFocus != null)
+        {
+            var imm = (Android.Views.InputMethods.InputMethodManager?)activity.GetSystemService(Android.Content.Context.InputMethodService);
+            imm?.HideSoftInputFromWindow(activity.CurrentFocus.WindowToken, Android.Views.InputMethods.HideSoftInputFlags.None);
+            activity.CurrentFocus.ClearFocus();
+        }
+#endif
+            await PageScrollView.ScrollToAsync(ContentPanel, ScrollToPosition.MakeVisible, true);
+        }
+
+    }
+}
